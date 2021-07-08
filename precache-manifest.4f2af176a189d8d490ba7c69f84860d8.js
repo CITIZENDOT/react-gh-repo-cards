8b0686328c2a572478a7e09dd3e8f95d3d5dddf3
CITIZENDOT <52322531+CITIZENDOT@users.noreply.github.com>
2021-07-08 10:28:22 +0530

    Updates

---
+++ precache-manifest.4f2af176a189d8d490ba7c69f84860d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7485f9e6182da3563b542948c54ec8ff",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f251947f4823aca2f489",
+    "url": "./static/css/main.dcbc12bb.chunk.css"
+  },
+  {
+    "revision": "e61c88aefb2384bb439d",
+    "url": "./static/js/2.f2ca5d61.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f2ca5d61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f251947f4823aca2f489",
+    "url": "./static/js/main.85244c8d.chunk.js"
+  },
+  {
+    "revision": "283e5eeac17087600af6",
+    "url": "./static/js/runtime-main.c39edcce.js"
+  }
+]);
